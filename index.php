83c3904f8ff3c19230b7af6f65bbbd40a23fcd02
Patrick McGuire <insidenothing@gmail.com>
2018-01-09 22:21:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ Parameters:
 */
 $id = 8;
 $key = $_SESSION['key'];
-$orders_json = pull("https://api.nicehash.com/api?method=orders.get&my&id=$id&key=$key&location=1&algo=1");
+$orders_json = pull("https://api.nicehash.com/api?method=orders.get&myid=$id&key=$key&location=1&algo=1");
 ?>
 
 
